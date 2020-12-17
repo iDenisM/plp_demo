14c218d34f4b100d55cb8d3f90939b5908ba38d5
BuildTools <denis.mitiliuc@gmail.com>
2020-12-17 22:41:19 +0100

    add the props for slider

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,29 +1,36 @@
 import './Slider.css'
+import React from 'react';
 
 type SliderProps = {
   images: string[]
 }
 
-const Slider = (props: SliderProps) => (
-  <div>
-    <button>
-      <span className="text">Previous slide</span>
-      <span className="icon"></span>
-    </button>
-    <div className="wrapper">
-      {
-        props.images.map(img => (
-          <div className="slide">
-            <img src={img} alt="product variant image"/>
-          </div>
-        ))
-      }
-    </div>
-    <button>
-      <span className="text">Next slide</span>
-      <span className="icon"></span>
-    </button>
-  </div>
-)
+export default class Slider extends React.Component<SliderProps, {}> {
+  constructor(props: SliderProps) {
+    super(props)
+  }
 
-export default Slider;
+  render() {
+    return (
+      <div>
+        <button>
+          <span className="text">Previous slide</span>
+          <span className="icon"></span>
+        </button>
+        <div className="wrapper">
+          {
+            this.props.images.map(img => (
+              <div className="slide">
+                <img src={img} alt="product variant"/>
+              </div>
+            ))
+          }
+        </div>
+        <button>
+          <span className="text">Next slide</span>
+          <span className="icon"></span>
+        </button>
+      </div>
+    )
+  }
+}
