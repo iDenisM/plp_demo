6d6b4508119c14614b1ac697de3947483e0dabc1
BuildTools <denis.mitiliuc@gmail.com>
2020-12-17 22:03:43 +0100

    add more element to slider

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -3,10 +3,10 @@ import Slider from '../slider/Slider';
 
 const Product = () => (
   <div>
-    <img src="https://via.placeholder.com/600" alt="image of the product name"></img>
+    <img src="https://via.placeholder.com/600" alt="product name" />
     <div>Produc Price</div>
     <div>Produc Name</div>
-    <Slider />
+    <Slider images={['https://via.placeholder.com/80x50', 'https://via.placeholder.com/80x50', 'https://via.placeholder.com/80x50']} />
   </div>
 );
 
--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,10 +1,28 @@
 import './Slider.css'
 
-const Slider = () => (
+type SliderProps = {
+  images: string[]
+}
+
+const Slider = (props: SliderProps) => (
   <div>
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
+    <button>
+      <span className="text">Previous slide</span>
+      <span className="icon"></span>
+    </button>
+    <div className="wrapper">
+      {
+        props.images.map(img => (
+          <div className="slide">
+            <img src={img} alt="product variant image"/>
+          </div>
+        ))
+      }
+    </div>
+    <button>
+      <span className="text">Next slide</span>
+      <span className="icon"></span>
+    </button>
   </div>
 )
 
